587c5cbb96ff0e731f05b901dd88b43bf8a97af4
sSim0o <bounakhlamed3@gmail.com>
2026-02-16 19:46:40 -0500

    Remove scrollToSection function and event listener
    Removed the scrollToSection function and scroll event listener.
    
    Signed-off-by: sSim0o <bounakhlamed3@gmail.com>

--- script.js
+++ script.js
@@ -14,13 +14,3 @@ function generateRandomScore() {
   else if (r < w) msg.textContent = "😔 Défaite...";
   else msg.textContent = "🤝 Match nul";
 }
-function scrollToSection(id) { 
-  document.getElementById(id).scrollIntoView({ behavior: 'smooth' });
- } 
- const sections = document.querySelectorAll('.section');
-  window.addEventListener('scroll', () => { sections.forEach(section => { const rect = section.getBoundingClientRect();
-     if(rect.top < window.innerHeight - 100){ 
-      section.classList.add('visible');
-     } 
-    });
-  });
